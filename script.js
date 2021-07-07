a6c94fada74074a0689af357c93756da80626222
Riley Lathan <rileylatha202@gmail.com>
2021-07-06 23:50:24 -0400

    updating initial files and folder

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+
+var symbolCharacters = [ "@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", ".", ];
+
+var numberCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var lowerCaseLetters = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ];
+
+var upperCaseLetters = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ];
+
+const fullPassword = symbolCharacters.concat(numberCharacters, lowerCaseLetters, upperCaseLetters);
+
+
+function generatePassword() { 
+  // Checks if password is between 8-128 characters.
+var passwordLength = prompt("Enter length of password.")
+while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) passwordLength = Number(prompt("Length must be between 8-128 characters."));
+
+// Check if Symbols are included in the password.
+var containsymbolCharacters = confirm("Click OK to confirm special characters in your password.")
+console.log(containsymbolCharacters);
+
+// Check if Numbers are included in the password.
+var containnumberCharacters = confirm("Click OK to confirm numeric characters in your password.")
+console.log(containnumberCharacters);
+
+// Check if lower case letters are included in the password.
+var containlowerCaseCharacters = confirm("Click OK to confirm lower cased characters in your password.")
+console.log(containlowerCaseCharacters);
+
+// Check if upper case letters are included in the password.
+var containupperCaseCharacters = confirm("Click OK to confirm upper cased characters in your password.")
+console.log(containupperCaseCharacters);
+
+for (var i = 0; i < passwordLength; i++){
+const password = fullPassword[Math.floor(Math.random() * fullPassword.length)];
+}
+return password;
+}
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
